b35444cc22dcc3485509cebd5d3c9ff1557025d0
AllocatorXy <allocatorxy@foxmail.com>
2017-04-23 15:13:00 +0800

    now scrTo has been compatible with bind function

--- dist/scrTo.js
+++ dist/scrTo.js
@@ -51,6 +51,27 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
   Object.prototype.removeEv = function (ev, fn, capture) {
     this.removeEventListener ? this.removeEventListener(ev, fn, capture) : this.detachEvent('on' + ev, fn);
   };
+
+  if (!Function.prototype.bind) {
+    Function.prototype.bind = function (oThis) {
+      if (typeof this !== 'function') {
+        // closest thing possible to the ECMAScript 5
+        // internal IsCallable function
+        throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+      }
+      var aArgs = Array.prototype.slice.call(arguments, 1),
+          fToBind = this,
+          fNOP = function fNOP() {},
+          fBound = function fBound() {
+        return fToBind.apply(this instanceof fNOP ? this : oThis || this, aArgs.concat(Array.prototype.slice.call(arguments)));
+      };
+
+      fNOP.prototype = this.prototype;
+      fBound.prototype = new fNOP();
+      return fBound;
+    };
+  }
+
 })();
 
 var easing = {
